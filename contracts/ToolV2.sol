c4a79237e5c7708f42de7e97fd959910bf837cf7
NanezX <victor.nanezj@gmail.com>
2021-05-08 16:18:55 -0400

    .

--- contracts/ToolV2.sol
+++ contracts/ToolV2.sol
@@ -3,5 +3,9 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
 contract ToolV2 is Initializable{
+    address payable recipient; // Recipient that get the fees
 
+    function initialize(address payable _recipient) public initializer {
+        recipient = _recipient;
+    }
 }
--- test/ToolV2.test.js
+++ test/ToolV2.test.js
@@ -11,16 +11,23 @@ const ACCOUNT = "0xbF334f8BD1420a1CbFE15407f73919424934B1B3"; // This account wi
 const altAcc = "0x4Ef88F266D03eC2a3e3e1beb1D77cB9c52c93003"; // This account will receive the fees (recipient address)
 const ALCHEMY_KEY = "7rjyfJ9o5dWSND5dUhl1sfFjQpG24BlV";
 
-describe("Transaction Router UNISWAP", ()=>{
+describe("Transaction v2", ()=>{
     let ToolV1;
     let instanceToolV1;
     let signer;
     let signerALT;
 
     beforeEach(async ()=>{ 
+        // Deploying
         ToolV1 = await ethers.getContractFactory("ToolV1");
         instanceToolV1 = await upgrades.deployProxy(ToolV1, [altAcc]);
         await instanceToolV1.deployed();
+
+        // Upgrading
+        const ToolV2 = await ethers.getContractFactory("ToolV2");
+        const instanceUpgraded = await upgrades.upgradeProxy( instanceToolV.address, ToolV2);
+
+
  
         await hre.network.provider.request({
             method: "hardhat_impersonateAccount",
@@ -47,31 +54,36 @@ describe("Transaction Router UNISWAP", ()=>{
     });
 
     describe("\n *-* CONTEXT v2: Swapping from ETH to one token", ()=>{
+        before(async()=>{
+            await restartFork();
+        });
         it("Swapping to DAI v2", async ()=>{
-             /* Overrides in ether.js is a parameter that can contain some properties of the transaction (from, to, value, ...)
-             In my test, only contain the "value" (ETH) that the ACCOUNT will send ;) */
             let overrides = { 
-                // To convert Ether to Wei:
                 value: ethers.utils.parseEther("1"),
             };
-            /*  IMPORTANT NOTE: It's RELEVANT to know that i use bips to manage percentages.
-             You may already know it, but 10000 bips is like 100% ; 5000 is 50% ; 10 is 0.1%; etc etc
-             So, the input to "percentages" must be set into a range of [0 - 10000] */
             let tx = await instanceToolV1.connect(signer).swapETHForTokens(
-                [DAI_ADDRESS], // Token address
-                [10000], // Percentages (10000 = 100%)
+                [DAI_ADDRESS],
+                [10000],
                 overrides 
-            ); 
-            // Waiting that it's mined  
-            tx = await tx.wait();        
-            // Getting Token balance
+            );  
+            tx = await tx.wait();    
+
             const DAI_ERC20 = await ethers.getContractAt("IERC20", DAI_ADDRESS);
             const balance = (await DAI_ERC20.balanceOf(ACCOUNT));
             console.log("\n1. Getting the balance of DAI: "+balance.toString());
-            // Gas used :)
             console.log("Gas Used:", (tx.gasUsed).toString());
-            // Checking if the correct fee was sending to recipient
             expect(await signerALT.getBalance()).to.equal(ethers.utils.parseEther("0.001"));
         });
     });
 });
+const restartFork = async ()=>{
+   await hre.network.provider.request({
+       method: "hardhat_reset",
+       params: [{
+           forking: {
+           jsonRpcUrl: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_KEY}`,
+           blockNumber: 12391206
+           }
+       }]
+   });
+};
