267c5d8020a666c69381e167de0c7931ce216465
NanezX <victor.nanezj@gmail.com>
2021-05-09 17:54:05 -0400

    Re-added the IERC20 to tests

---
+++ contracts/interfaces/IERC20.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+abstract contract imports {}
+
+// This will get the artifact of IERC20 to test the contracts
